1c334f68ef1a4ab4da15928cc0c075e84eee501a
jesse <xiongjiexi@qq.com>
2021-06-07 00:22:12 +0800

    05_spring框架homework：主动和被动两种方式让starter生效

--- 05_spring-boot-starter/src/main/java/org/example/EnableSchool.java
+++ 05_spring-boot-starter/src/main/java/org/example/EnableSchool.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
+/**
+ * 使用enable注解，是主动生效的方式，必须要使用此注解生效
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- 05_spring_school_project/src/main/java/org/example222/ApplicationStarter.java
+++ 05_spring_school_project/src/main/java/org/example222/ApplicationStarter.java
@@ -1,13 +1,12 @@
 package org.example222;
 
-import org.example.EnableSchool;
 import org.example.School;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@EnableSchool
+//@EnableSchool
 public class ApplicationStarter {
 
     public static void main(String[] args) {
